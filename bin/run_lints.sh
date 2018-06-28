5f626f45c798f8332fb622ad0b710733ac194797
Michael Kelly <mkelly@mozilla.com>
2018-06-28 16:42:23 -0700

    Add eslint for linting.

---
+++ bin/run_lints.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Runs linting.
+
+set -e
+
+# Run flake8 to lint Python tests
+echo ">>> eslint"
+eslint .
+
+echo ">>> web-ext lints"
+web-ext lint --self-hosted --source-dir src
